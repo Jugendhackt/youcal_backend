974c20f09b15e48748ceadaa4dc9600de964cfcf
HuhnCares <huhncares@gmail.com>
2023-05-06 10:43:38 +0200

    Basic fastapi structure

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-def say_hello(greet):
-    return f"Hello, {greet}!"
+import fastapi
 
-if __name__ == "__main__":
-    print(say_hello("World"))
+app = fastapi.FastAPI()
+
+@app.get("/")
+def index():
+    return {"message": "Hello, world!"}
+
+# Matplotlib: Generate graph and return image link
+@app.get("/api/v1/graph_image")
+def calculate(topic: str):
+    return {"message": "Image."}
+
+# Finds out the most relevant keywords
+@app.get("/api/v1/keywords")
+def keywords(topic: str):
+    return {"message": "Keywords!"}
+
+# ChatGPT-Summary: Summarizes the Comments and Posts
+@app.get("/api/v1/summary")
+def summary(topic: str):
+    return {"message": "Summary!"}
